7973008ecb198d0f3b5414c5928ac4d088e4653c
Josh Booker <josh.booker2@gmail.com>
2023-12-30 15:32:35 +0000

    formatted date and writing page in general

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -4,8 +4,11 @@ import MenuTitle from '@/components/MenuTitle';
 import ModalContainer from '@/components/ModalContainer';
 import Paragraph from '@/components/Paragraph';
 import Title from '@/components/Title';
-import { Container } from 'postcss';
+import Container from '@/components/Container';
 import React, { useEffect, useState } from 'react';
+import Pulse from '@/components/Pulse';
+import Button from '@/components/Button';
+import Modal from '@/components/Modal';
 
 interface BlogPost {
 	id: number;
@@ -20,6 +23,24 @@ interface BlogPost {
 
 const WritingPage = () => {
 	const [blogPosts, setBlogPosts] = useState<null | BlogPost[]>([]);
+	const reverseString = (string: string) => {
+		let reversedString = '';
+		for (let i = string.length - 1; i >= 0; i--) {
+			reversedString += string[i];
+		}
+		return reversedString;
+	};
+	const formatDate = (blogPosts: BlogPost[]) => {
+		for (let blog of blogPosts) {
+			let date = blog.date.substring(0, 10);
+			let dateArray = date.split('-');
+			let formattedDate = '';
+			for (let i = 2; i >= 0; i--) {
+				formattedDate += `${dateArray[i]} `;
+			}
+			return formattedDate;
+		}
+	};
 
 	const getBlogPosts = () => {
 		fetch('http://localhost:8888/wordpress/wp-json/wp/v2/posts')
@@ -49,9 +70,10 @@ const WritingPage = () => {
 	useEffect(() => {
 		getBlogPosts();
 	}, []);
-	if (blogPosts) {
-		console.log(blogPosts[0].date);
-	}
+	// if (blogPosts) {
+	// 	console.log(blogPosts[0].date);
+	// }
+
 	return (
 		<div className="flex flex-col items-center my-10 mx-20 max-w-screen">
 			<MenuTitle>My Little Dev Blog</MenuTitle>
@@ -62,11 +84,15 @@ const WritingPage = () => {
 				I can use RESTful APIs!
 			</Paragraph>
 			<LineBreak />
-			{blogPosts &&
+			{blogPosts ? (
 				blogPosts.map((blogpost) => (
 					<div key={blogpost.id}>
 						<Title>{blogpost.title.rendered}</Title>
-						<Paragraph>{blogpost.date}</Paragraph>
+						<Paragraph>
+							<span className="font-bold mb-8">
+								{formatDate(blogPosts)}
+							</span>
+						</Paragraph>
 						<Paragraph>
 							<div
 								dangerouslySetInnerHTML={{
@@ -76,7 +102,21 @@ const WritingPage = () => {
 						</Paragraph>
 						<LineBreak />
 					</div>
-				))}
+				))
+			) : (
+				<Container>
+					<Title>
+						<Pulse>One sec, just fetching some stuff!</Pulse>
+					</Title>
+					<Paragraph>
+						If nothing shows up, you can click{' '}
+						<Modal>
+							<a href="">here</a>
+						</Modal>{' '}
+						to be taken to my wordpress blog!
+					</Paragraph>
+				</Container>
+			)}
 		</div>
 	);
 };
